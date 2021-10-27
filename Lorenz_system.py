5002fb498fd9679b7af93ea71063c61621a08964
fredreak <fredreak@stud.ntnu.no>
2021-10-27 22:38:23 +0200

    Bytter PC, updater

--- Lorenz_system.py
+++ Lorenz_system.py
@@ -5,38 +5,29 @@ Created on Wed Nov 18 14:37:15 2020
 @author: fredr
 """
 import numpy as np
-#import num_int_adaptive as nia
 from random import random
 from scipy.stats import linregress
 from scipy.integrate import quad
-
 import matplotlib.pyplot as plt
-from scipy.signal import argrelextrema 
-from random import random
+#from scipy.signal import argrelextrema 
 
-
+#SET OF EQUATIONS
 def x_deriv(x, y, z, sigma):
     return sigma*(y - x)
-
 def y_deriv(y, z, x, rho):
     return (x*(rho-z)-y)
-
 def z_deriv(z, x, y, beta):
     return (x*y-beta*z)
 
 def update_values(x, y, z, sigma, rho, beta, delta, TOL):
-#    x_new = x + nia.adap_q(x, x+delta, x_deriv, nia.simpsons, TOL, y, z, sigma)
-#    y_new = y + nia.adap_q(y, y+delta, y_deriv, nia.simpsons, TOL, z, x, rho)
-#    z_new = z + nia.adap_q(z, z+delta, z_deriv, nia.simpsons, TOL, x, y, beta)   
     x_new = x + quad(x_deriv, x, x+delta, (y,z,sigma), epsrel = 1e-3)[0]
     y_new = y + quad(y_deriv, y, y+delta, (z,x,rho), epsrel = 1e-3)[0]
     z_new = z + quad(z_deriv, z, z+delta, (x, y, beta), epsrel = 1e-3)[0]
     return x_new, y_new, z_new
 
-
 def run_lorenz(x_0, y_0, z_0, t_f, delta, sigma, rho, beta, TOL):
     N = int(t_f/delta) - 1        #Number of iterations 
-    X, Y, Z, T = [np.zeros(N+1), np.zeros(N+1), np.zeros(N+1), np.zeros(N+1)]
+    X, Y, Z, T = [np.zeros(N+1), np.zeros(N+1), np.zeros(N+1), np.zeros(N+1)] #Variables
     X[0],Y[0], Z[0] = x_0, y_0, z_0 #Set initial conditions
     for i in range(N):
         X[i+1], Y[i+1], Z[i+1] = update_values(X[i], Y[i], Z[i], sigma, rho, beta, delta, TOL)
@@ -45,9 +36,9 @@ def run_lorenz(x_0, y_0, z_0, t_f, delta, sigma, rho, beta, TOL):
 
 
 #Task 2.2c i
-def point_after_time(t_start, delta, sigma, rho, beta, TOL): #Define x_0 as the point reached after "t_start"
-    x, y, z, _ = run_lorenz(random()*1e-5, random()*1e-5, random()*1e-5, t_start, delta, sigma, rho, beta, TOL)
-    return np.array([x[-1], y[-1], z[-1]])
+def point_after_time(t_start, delta, sigma, rho, beta, TOL): #Define x_0 as the point reached after "t_start" starting at random location. Used to create a set of random start positions that reflects the dist. of the set of eq's
+    x, y, z, _ = run_lorenz(random()*1e-4, random()*1e-4, random()*1e-4, t_start, delta, sigma, rho, beta, TOL)
+    return np.array([x[-1], y[-1], z[-1]]) #Returning only final spatial position
 
 #Task 2.2c ii
 def point_in_neighbourhood(point_0, d): #Returns a random vector at a distance "d" from x_0
@@ -80,13 +71,44 @@ def liapunov_constant(T_c, delta, sigma, rho, beta, TOL, N): #N = number of iter
         liapunov_array[n], _, _, _, _ = linregress(sep, T)
     return liapunov_array.mean(), sep, T
 
-def generate_points(t_simulate, N, delta, sigma, rho, beta, TOL): #returns a Nx3 matrix with locations of the points
-    particles = np.empty((0,3)) #A 2D-array with locations of all "particles", each defined 3 spatial coordinates
-    particles = np.append(particles, np.array([point_after_time(50, delta, sigma, rho, beta, TOL)]), axis = 0) #Append a single point on the attractor to particles. Point found using point_after_time
-    for i in range(N-1):
-        new_particle = point_in_neighbourhood(particles[0], 1e-2) #Generate a new random point in the neighbourhood of first point
-        x_1, y_1, z_1, _ = run_lorenz(new_particle[0], new_particle[1], new_particle[2], 0, t_simulate, delta, sigma, rho, beta, TOL) #Time evolve this new point
-        particles = np.append(particles, np.array([[x_1[-1], y_1[-1], z_1[-1]]]), axis = 0) #Append the new point to the set of particles
+#NEW TASK: FRACTAL DIMENSION
+def write_particles_to_file(particles):
+    """
+    "x" - Create - will create a file, returns an error if the file exist
+    "a" - Append - will create a file if the specified file does not exist
+    "w" - Write - will create a file if the specified file does not exist
+    """
+    try:
+        file_obj = open("particles.txt", "x")
+        print("Creating new file ...")
+    except:
+        file_obj = open("particles.txt", "a")
+        print("Appending existing file ...")
+    finally:
+        for particle in particles:
+            file_obj.write(str(particle)+"\n")
+        file_obj.close()
+        print("File closed.")
+    return 1
+
+def read_particles_from_file(): #USING with WHICH CALLS __enter(self)__ ON CONSTRUCTING THE CLASS OBJECT AND __exit__(...) UPON EXITING (https://www.youtube.com/watch?v=sJnXN1lLodY) ==> NO NEED TO CLOSE FILE
+    try: 
+        with open("particles.txt", "r") as file_obj:
+            print("Reading from file...")
+            particles = np.empty((0,3))
+            for particle in file_obj:
+                particle = np.fromstring(particle[1:-2], dtype = float, sep=" ")
+                particles = np.append(particles,np.array([particle]), axis = 0)
+        print("File closed.")
+    except Exception as e:
+        print("Exception occured when reading file, message:\n", e)   
+    return particles
+
+def generate_points(t_simulate, N, delta, sigma, rho, beta, TOL): #Generates new particles and saves them to file. Returns new particles. 
+    particles = np.empty((0,3)) #A 2D-array with locations of all "particles", each defined 3 spatial coordinates (Particles saved as Nx3 matrix with locations of the points)
+    for i in range(N):
+        particles = np.append(particles, np.array([point_after_time(t_simulate, delta, sigma, rho, beta, TOL)]), axis = 0) #Uses point_after_time to append new points (ONLY SPATIAL POINTS)
+    write_particles_to_file(particles)
     return particles 
 
 def plot(array_1, array_2, label_x, label_y, style = "o-", c = "blue"):
@@ -95,16 +117,45 @@ def plot(array_1, array_2, label_x, label_y, style = "o-", c = "blue"):
     plt.plot(array_1, array_2, style, color = c)
     return 1
 
+def scatter_plot_2(particles, ax1, ax2):
+    plt.figure()
+#    plt.xlabel('x')
+#    plt.ylabel('y')
+    particles = particles.transpose()
+    plt.scatter(particles[ax1], particles[ax2])
+    plt.show()
+
+
+
+#########################################################
+    
 def work_station():
-    x_0, y_0, z_0 = [0.001]*3 #Set Initial values
     delta, TOL = 1e-3, 1e-4  #Set partition delta and quadrature error tolerance TOL
-    sigma, rho, beta = 10, 28, 8
-    t_limit = 40
-    x,y,z,T = run_lorenz(x_0, y_0, z_0, t_limit, delta, sigma, rho, beta, TOL)
-    plot()
+    sigma, rho, beta = 10, 28, 8/3
+    
+    #Generate new particles
+    if (1): 
+        N = 3
+        t_simulate = 35
+        generate_points(t_simulate, N, delta, sigma, rho, beta, TOL)
+        print(N, "particles appended")
 
+    #Read exisiting particles from file
+    all_particles = read_particles_from_file()
 
-print("Ran as ", __name__)
+    #PLot particles
+    scatter_plot_2(all_particles, 0, 2)
+    
+    #Test lorenz:
+#    x_0, y_0, z_0 = [0.001]*3 #Set Initial values
+#    x,y,z,T = run_lorenz(x_0, y_0, z_0, t_f, delta, sigma, rho, beta, TOL)
+#    plot(x,z,"x","z","-")
+#    plt.legend(["Lorenz system. Sigma = {}, Rho = {}, Beta = {}".format(10, 28, round(8/3,2))])
+#    plt.show()
+
+    return 1
+
+print("Lorenz_system ran as ", __name__)
 if __name__ == "__main__":
-    work_station()
-    return 0
+    work_station() 
+    
